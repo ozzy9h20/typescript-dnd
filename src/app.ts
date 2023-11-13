428513454ceebb874c61f61f132930098bd62cc6
M. Endyarroziqin <endy@millennia-solusi.id>
2023-11-13 15:45:23 +0700

    Section 09 - Chapter 130 ~ Filtering Projects with Enums

--- src/app.ts
+++ src/app.ts
@@ -114,7 +114,13 @@ class ProjectList {
     this.assignedProjects = []
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active
+        }
+        return prj.status === ProjectStatus.Finished
+      })
+      this.assignedProjects = relevantProjects
       this.renderProjects()
     })
 
@@ -124,6 +130,7 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement
+    listEl.innerHTML = ''
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li')
       listItem.textContent = prjItem.title
