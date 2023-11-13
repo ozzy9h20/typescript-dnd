3000001829bf9cfe9ca1d3fef06278cfe5586a64
M. Endyarroziqin <endy@millennia-solusi.id>
2023-11-13 20:23:39 +0700

    Section 10 - Chapter 143 ~ Organizing Files & Folders

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,12 @@
+namespace App {
+  export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        return originalMethod.bind(this)
+      }
+    }
+    return adjDescriptor
+  }
+}
